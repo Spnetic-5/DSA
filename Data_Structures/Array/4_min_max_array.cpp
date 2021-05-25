822230a14e15f58be0094d5658e0f194adb5a95e
Spnetic-5 <sspowar_b19@el.vjti.ac.in>
2021-05-26 01:42:21 +0530

    added kth_min_array

--- Data_Structures/Array/4_min_max_array.cpp
+++ Data_Structures/Array/4_min_max_array.cpp
@@ -1,57 +1,26 @@
-// (Simple Linear Search) 
-// Initialize values of min and max as minimum and maximum of the first two elements respectively. Starting from 3rd, compare each element with max and min, and change max and min accordingly (i.e., if the element is smaller than min then change min, else if the element is greater than max then change max, else ignore the element) 
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-struct Pair{
-    int min;
-    int max;
-};
-
-struct Pair getMinMax(int arr[], int n){
-    struct Pair minmax;
-    
-
-// if only one element in array
-    if(n==1){
-        minmax.max = arr[0];
-        minmax.min = arr[0];
-
+int main ()
+{
+    int arr[10], n, i, max, min;
+    cout << "Enter size ";
+    cin >> n;
+    cout << "Enter elements ";
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+    max = arr[0];
+    for (i = 0; i < n; i++)
+    {
+        if (max < arr[i])
+            max = arr[i];
     }
-
-// if more that one elements
-if (arr[0]>arr[1]){
-    minmax.max = arr[0];
-    minmax.min = arr[1];
+    min = arr[0];
+    for (i = 0; i < n; i++)
+    {
+        if (min > arr[i])
+            min = arr[i];
+    }
+    cout << "Largest element : " << max<<endl;
+    cout << "Smallest element : " << min<<endl;
+    return 0;
 }
-else{
-    minmax.max = arr[1];
-    minmax.min = arr[0];
-}
-
-for(int i=2;i<n;i++){
-    if (arr[i] > minmax.max)
-        minmax.max=arr[i];
-    else if (arr[i] < minmax.min)
-        minmax.min = arr[i];
-}
-return minmax;
-}
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int j=0;j<n;j++){
-        cin>>arr[j];
-    }    
-    
-    struct Pair minmax = getMinMax(arr, n);
-    cout << "Minimum is "
-         << minmax.min << endl;
-    cout << "Maximum is "
-         << minmax.max<< endl;
-
-return 0;
-
-}
-
---
+++ Data_Structures/Array/5_kth_smallest_array.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+ 
+int kthSmallest(int arr[], int n, int k){
+sort(arr, arr + n);
+return arr[k - 1];
+}
+int main(){
+    int arr[] = { 1, 3, 5, 7};
+    int n = sizeof(arr) / sizeof(arr[0]), k = 2;
+    cout << "K'th smallest element is " << kthSmallest(arr, n, k);
+    return 0;
+}
