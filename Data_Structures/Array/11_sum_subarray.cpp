6d31b85357b5b73a0e9bd1018fd6c06ae2a33cbf
Spnetic-5 <saurabhpowar1823@gmail.com>
2021-05-28 12:52:46 +0530

    added lenght of longest arithmetic subarray

---
+++ Data_Structures/Array/11_sum_subarray.cpp
@@ -0,0 +1,31 @@
+// Time Complexity : 
+//  ------------------------------------------ SUM OF ALL SUBARRAYS -----------------------------------------------
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+
+    int sum=0;
+
+    for(int i=0;i<n;i++){ 
+        sum=0;
+        for(int j=i;j<n;j++)    {
+
+            sum += arr[j];
+            cout<<sum<<" ";    
+        }
+     cout<<endl;   
+    }
+    // cout<<sum;
+    
+    return 0;
+}
