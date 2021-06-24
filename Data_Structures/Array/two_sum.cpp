8f4023b25fad948ee51e0e06ad1a9c887cb2d04b
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-06-24 21:14:59 +0530

    added Pair sum problem

--- Data_Structures/Array/two_sum.cpp
+++ Data_Structures/Array/two_sum.cpp
@@ -1,25 +1,41 @@
-// ------------------------------------------------------------------- TWO SUM PROBLEM --------------------------------------------------------------------------------------
-// Time Complexity: O(n)
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
-
-int main(){
-
-    int sum = 15;
-    vector<int> arr={5, 6, 7, 8, 2, 4};
-
-    unordered_map<int, int> mp;
-
-    for (int i = 0; i < arr.size(); i++){
-        int rem = sum - arr[i];
-    
-    if(mp.find(rem) != mp.end()){
-        cout<< i << " " << mp[rem] << endl;  // if rem in map print indices
+ 
+// Function to find a pair in an array with a given sum using hashing
+void findPair(int arr[], int n, int sum)
+{
+    // create an empty map
+    unordered_map<int, int> map;
+ 
+    // do for each element
+    for (int i = 0; i < n; i++)
+    {
+        // check if pair `(arr[i], sum - arr[i])` exists
+ 
+        // if the difference is seen before, print the pair
+        if (map.find(sum - arr[i]) != map.end())
+        {
+            cout << "Pair found (" << arr[map[sum - arr[i]]] << ", " << arr[i] << ")";
+            return;
+        }
+ 
+        // store index of the current element in the map
+        map[arr[i]] = i;
     }
-    mp[arr[i]] = i; // insert element in map
-    }
-
-
+ 
+    // we reach here if the pair is not found
+    cout << "Pair not found";
+}
+ 
+int main()
+{
+    int arr[] = { 8, 7, 2, 5, 3, 1 };
+    int sum = 10;
+ 
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    findPair(arr, n, sum);
+ 
     return 0;
 }
